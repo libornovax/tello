a4df02df8e298cfe60f2aca25d8f7ec89378dbed
Libor Novak <libornovax@gmail.com>
2019-11-01 13:10:53 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from subprocess import check_output
+
+
+def setup_package():
+
+    # Determine package version from git
+    version = check_output("git describe --tags --dirty".split()).decode('utf-8').strip()
+
+    setup(
+
+        # ----------------------------------- PACKAGE METADATA ----------------------------------- #
+
+        name="tello",
+        version=version,
+        author="Libor Novak",
+        author_email="libornovax@gmail.com",
+
+        # --------------------------------- PACKAGE DEPENDENCIES --------------------------------- #
+
+        install_requires=[
+        ],
+        setup_requires=[
+            "pytest-runner",
+        ],
+        tests_require=[
+            "pytest",
+            "pytest-cov"
+        ],
+
+        # ----------------------------------- PACKAGE CONTENTS ----------------------------------- #
+
+        packages=find_packages("src"),
+        package_dir={
+            "": "src"
+        },
+        package_data={
+        },
+
+        # ----------------------------------- PACKAGE SCRIPTS ------------------------------------ #
+
+        entry_points={
+        }
+
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
